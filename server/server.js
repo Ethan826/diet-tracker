1b6198fef08b5738cde1fa54225316a9f022fb12
Ethan Kent <ekent@jd11.law.harvard.edu>
2016-03-01 12:05:25 -0600

    continued tls

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 "use strict";
 var express = require("express");
+var serveIndex = require("serve-index");
 var app = express();
 app.use("/app", express.static(__dirname + "/../app"));
 app.use("/node_modules", express.static(__dirname + "/../node_modules"));
-app.use("/.well-known", express.static(__dirname + "/../.well-known"));
+app.use("/.well-known", serveIndex(__dirname + "/../.well-known"));
 var PORT = 8080;
 app.get("/", function (req, res) {
     res.sendFile("/index.html", { "root": __dirname + "/../" });
-});
-app.get("/.well-known", function (req, res) {
-    res.sendFile("/.well-known");
 });
 app.listen(PORT, function () {
     console.log("Listening on " + PORT);
--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,18 @@
 /// <reference path="./typings/main/ambient/node/node.d.ts"/>
 import * as express from "express";
+let serveIndex = require("serve-index");
 
 let app = express();
 
 app.use("/app", express.static(`${__dirname}/../app`));
 app.use("/node_modules", express.static(`${__dirname}/../node_modules`));
-app.use("/.well-known", express.static(`${__dirname}/../.well-known`))
+app.use("/.well-known", serveIndex(`${__dirname}/../.well-known`))
 
 let PORT = 8080;
 
 app.get("/", (req, res) => {
   res.sendFile("/index.html", {"root": __dirname + "/../"});
 });
-
-app.get("/.well-known", (req, res) => {
-  res.sendFile("/.well-known")
-});
-
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);

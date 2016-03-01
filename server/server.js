e5c14a1b257419e2d0351075983635ae21c0f71e
Ethan Kent <ekent@jd11.law.harvard.edu>
2016-03-01 10:44:41 -0600

    Implementing letsencrypt

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 "use strict";
 var express = require("express");
 var app = express();
-var PORT = 8080;
 app.use("/app", express.static(__dirname + "/../app"));
 app.use("/node_modules", express.static(__dirname + "/../node_modules"));
+app.use("/.well-known", express.static(__dirname + "/../.well-known"));
+var PORT = 8080;
 app.get("/", function (req, res) {
     res.sendFile("/index.html", { "root": __dirname + "/../" });
 });
 app.listen(PORT, function () {
-    console.log("Listening on port " + PORT);
+    console.log("Listening on " + PORT);
 });
--- server/server.ts
+++ server/server.ts
@@ -3,15 +3,16 @@ import * as express from "express";
 
 let app = express();
 
-let PORT = 8080;
+app.use("/app", express.static(`${__dirname}/../app`));
+app.use("/node_modules", express.static(`${__dirname}/../node_modules`));
+app.use("/.well-known", express.static(`${__dirname}/../.well-known`))
 
-app.use("/app", express.static(__dirname + "/../app"));
-app.use("/node_modules", express.static(__dirname + "/../node_modules"));
+let PORT = 8080;
 
 app.get("/", (req, res) => {
   res.sendFile("/index.html", {"root": __dirname + "/../"});
 });
 
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+  console.log(`Listening on ${PORT}`);
 });

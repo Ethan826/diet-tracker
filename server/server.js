f8947cbd0e95ac022b35a9fab112ee265e8b536f
Ethan Kent <ekent@jd11.law.harvard.edu>
2016-03-01 15:43:31 -0600

    Implmenting tls

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ var serveIndex = require("serve-index");
 var app = express();
 app.use("/app", express.static(__dirname + "/../app"));
 app.use("/node_modules", express.static(__dirname + "/../node_modules"));
-app.use("/.well-known", serveIndex(__dirname + "/../.well-known"));
 var PORT = 8080;
 app.get("/", function (req, res) {
     res.sendFile("/index.html", { "root": __dirname + "/../" });
+});
+app.get("/.well-known/acme-challenge/l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM", function (req, res) {
+    res.send("l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM.bLOPm_rh6Oz2YvKm5McIcpMVetZGeMEjQ40fFLN5fdI");
 });
 app.listen(PORT, function () {
     console.log("Listening on " + PORT);
--- server/server.ts
+++ server/server.ts
@@ -6,12 +6,15 @@ let app = express();
 
 app.use("/app", express.static(`${__dirname}/../app`));
 app.use("/node_modules", express.static(`${__dirname}/../node_modules`));
-app.use("/.well-known", serveIndex(`${__dirname}/../.well-known`))
 
 let PORT = 8080;
 
 app.get("/", (req, res) => {
   res.sendFile("/index.html", {"root": __dirname + "/../"});
+});
+
+app.get("/.well-known/acme-challenge/l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM", (req, res) => {
+  res.send("l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM.bLOPm_rh6Oz2YvKm5McIcpMVetZGeMEjQ40fFLN5fdI");
 });
 
 app.listen(PORT, () => {

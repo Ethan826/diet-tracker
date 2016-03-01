c68c245e30a2484878da71c6694bc5d6ca224ee7
Ethan Kent <ekent@jd11.law.harvard.edu>
2016-03-01 16:19:36 -0600

    Continued implementing TLS

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 "use strict";
 var express = require("express");
-var serveIndex = require("serve-index");
+var https = require("https");
+var fs = require("fs");
 var app = express();
+var HTTPS_PORT = 4430;
 app.use("/app", express.static(__dirname + "/../app"));
 app.use("/node_modules", express.static(__dirname + "/../node_modules"));
-var PORT = 8080;
 app.get("/", function (req, res) {
     res.sendFile("/index.html", { "root": __dirname + "/../" });
 });
-app.get("/.well-known/acme-challenge/l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM", function (req, res) {
-    res.send("l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM.bLOPm_rh6Oz2YvKm5McIcpMVetZGeMEjQ40fFLN5fdI");
-});
-app.listen(PORT, function () {
-    console.log("Listening on " + PORT);
-});
+https.createServer({
+    key: fs.readFileSync("../../secrets/flashbangsplat/privkey.pem"),
+    cert: fs.readFileSync("../../secrets/flashbangsplat/cert.pem")
+}, app).listen(HTTPS_PORT);
--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,27 @@
 /// <reference path="./typings/main/ambient/node/node.d.ts"/>
 import * as express from "express";
-let serveIndex = require("serve-index");
+import * as https from "https";
+import * as fs from "fs";
 
 let app = express();
 
+let HTTPS_PORT = 4430;
+
 app.use("/app", express.static(`${__dirname}/../app`));
 app.use("/node_modules", express.static(`${__dirname}/../node_modules`));
-
-let PORT = 8080;
 
 app.get("/", (req, res) => {
   res.sendFile("/index.html", {"root": __dirname + "/../"});
 });
 
-app.get("/.well-known/acme-challenge/l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM", (req, res) => {
-  res.send("l4iDo50t4F2RCS0d3wB4KEIYJe-urHb5FtHDGUPkJEM.bLOPm_rh6Oz2YvKm5McIcpMVetZGeMEjQ40fFLN5fdI");
-});
+https.createServer({
+  key: fs.readFileSync("../../secrets/flashbangsplat/privkey.pem"),
+  cert: fs.readFileSync("../../secrets/flashbangsplat/cert.pem")
+}, app).listen(HTTPS_PORT);
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+// let PORT = 8080;
+//
+//
+// app.listen(PORT, () => {
+//   console.log(`Listening on ${PORT}`);
+// });

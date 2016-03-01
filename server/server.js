f530d9c265d79260220a3a7b1aca866415b48a46
Ethan Kent <ekent@jd11.law.harvard.edu>
2016-03-01 17:24:29 -0600

    HTTP server redirecting to HTTPS.

--- server/server.js
+++ server/server.js
@@ -3,13 +3,23 @@ var express = require("express");
 var https = require("https");
 var fs = require("fs");
 var app = express();
+var insecure = express();
 var HTTPS_PORT = 8443;
+var HTTP_PORT = 8080;
+insecure.get("*", function (req, res) {
+    res.redirect("https://flashbangsplat.com" + req.url);
+});
+insecure.listen(HTTP_PORT, function () {
+    console.log("Listening on port " + HTTP_PORT);
+});
 var OPTIONS = {
-    key: fs.readFileSync("../../secrets/flashbangsplat/privkey.pem"),
-    cert: fs.readFileSync("../../secrets/flashbangsplat/cert.pem")
+    key: fs.readFileSync("/home/ethan/Desktop/temp/privkey.pem"),
+    cert: fs.readFileSync("/home/ethan/Desktop/temp/cert.pem")
 };
+app.use("/app", express.static(__dirname + "/../app"));
+app.use("/node_modules", express.static(__dirname + "/../node_modules"));
 app.get("/", function (req, res) {
-    res.send("Hello");
+    res.sendFile("/index.html", { "root": __dirname + "/../" });
 });
 https.createServer(OPTIONS, app).listen(HTTPS_PORT, function () {
     console.log("Listening on port " + HTTPS_PORT);
--- server/server.ts
+++ server/server.ts
@@ -1,37 +1,37 @@
 /// <reference path="./typings/main/ambient/node/node.d.ts"/>
 import * as express from "express";
 import * as https from "https";
+import * as http from "http";
 import * as fs from "fs";
 
 let app = express();
+let insecure = express();
 
 let HTTPS_PORT = 8443;
+let HTTP_PORT = 8080;
 
-let OPTIONS = {
-  key: fs.readFileSync("../../secrets/flashbangsplat/privkey.pem"),
-  cert: fs.readFileSync("../../secrets/flashbangsplat/cert.pem")
-  // key: fs.readFileSync("/home/ethan/Desktop/temp/privkey.pem"),
-  // cert: fs.readFileSync("/home/ethan/Desktop/temp/cert.pem")
-}
-
-app.get("/", (req, res) => {
-  res.send("Hello");
+insecure.get("*", (req, res) => {
+  res.redirect(`https://flashbangsplat.com${req.url}`);
 });
 
-// app.use("/app", express.static(`${__dirname}/../app`));
-// app.use("/node_modules", express.static(`${__dirname}/../node_modules`));
-//
-// app.get("/", (req, res) => {
-//   res.sendFile("/index.html", {"root": __dirname + "/../"});
-// });
+insecure.listen(HTTP_PORT, () => {
+  console.log(`Listening on port ${HTTP_PORT}`);
+});
+
+let OPTIONS = {
+  // key: fs.readFileSync("../../secrets/flashbangsplat/privkey.pem"),
+  // cert: fs.readFileSync("../../secrets/flashbangsplat/cert.pem")
+  key: fs.readFileSync("/home/ethan/Desktop/temp/privkey.pem"),
+  cert: fs.readFileSync("/home/ethan/Desktop/temp/cert.pem")
+}
+
+app.use("/app", express.static(`${__dirname}/../app`));
+app.use("/node_modules", express.static(`${__dirname}/../node_modules`));
+
+app.get("/", (req, res) => {
+  res.sendFile("/index.html", {"root": __dirname + "/../"});
+});
 
 https.createServer(OPTIONS, app).listen(HTTPS_PORT, () => {
   console.log(`Listening on port ${HTTPS_PORT}`);
 });
-
-// let PORT = 8080;
-//
-//
-// app.listen(PORT, () => {
-//   console.log(`Listening on ${PORT}`);
-// });
